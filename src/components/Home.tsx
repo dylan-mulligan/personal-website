f7a95be2e77870a23b72724e31f2da3e412754ee
Dylan Mulligan <dylan.mulligan2020@gmail.com>
2025-02-13 01:26:52 -0500

    Imported Blog Template

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import Blog from "./Blog";
+
+function Home() {
+    return (
+        <div className="Home">
+            <Blog/>
+        </div>
+    );
+}
+
+export default Home;
